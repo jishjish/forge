dbee7bb309af5429dcdad62e1c725228d7de8bbe
jishjish <josh@astrotek.app>
2026-05-02 13:17:55 -0400

    update cuda driver references and cuda pydantic model

--- src/forge/models.py
+++ src/forge/models.py
@@ -1,11 +1,15 @@
 from pydantic import BaseModel
 
-
-class GPU(BaseModel):
-    device_type: str
-    cuda_cores: int
-    streaming_multiprocessors: int
-    threads_per_block: int
+class CudaGPU(BaseModel):
+    version: str
+    arch: str                       # f string "fm_{major}{minor}" to be used for compiler reference
+    max_threads_per_block: int
+    max_block_dim_x: int
+    max_block_dim_y: int
+    max_block_dim_z: int
+    max_grid_dim_x: int
+    max_grid_dim_y: int
+    max_grid_dim_z: int
     warp_size: int
+    sm_count: int
     max_threads_per_sm: int
-    max_threads_total: int
