7155b86e0994915f9a0ac7083a5d330fe6bfaab0
jishjish <josh@astrotek.app>
2026-05-03 17:28:30 -0400

    error handling for device info request

--- src/forge/device/device_info.py
+++ src/forge/device/device_info.py
@@ -24,9 +24,15 @@ def get_device_info(device_type: str):
         _ops_file = [file for file in (Path(__file__).parent.parent/"ops").iterdir() if file.stem.startswith("ops_") and file.stem[len("ops_"):].upper() == device_type]
         module = importlib.import_module(f"forge.ops.{_ops_file[0].stem}")
         cls = getattr(module, f"_{device_type.capitalize()}Ops")
-        return cls()
-    except:
-        pass
+        req = cls()
+        return req._device_info()
+    except FileNotFoundError:
+        raise RuntimeError(f"No file found for device: {device_type}.")
+    except AttributeError:
+        raise RuntimeError(f"_{device_type.capitalize()}Ops class not found in module.")
+    except Exception as e:
+        raise RuntimeError(f"Failed to initialize device {device_type}: e")
+
 
 
     
