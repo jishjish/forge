52165b06fca2c306625b3b1863fdf73984b2dc53
jishjish <josh@astrotek.app>
2026-05-05 15:11:47 -0400

    consolidate device helper functions to Device class, delete device info file

--- src/forge/device/device.py
+++ src/forge/device/device.py
@@ -1,27 +1,90 @@
-from .device_info import get_device_type, get_device_info
+# from .device_info import get_device_type, get_device_info
+import ctypes
+import platform
+import importlib
+from pathlib import Path
+from dotenv import load_dotenv
+from ..constants.device import CHIPS 
+
+load_dotenv()
+
 
 class Device:
     def __init__(self):
-        self.supported_devices = [get_device_type()]
+        self.supported_devices = [self.get_device_type()]
 
     def _init_device(self):
         # call for the first device in supported devices
-        try: return get_device_info(self.supported_devices[0])
+        try: return self.get_device_info(self.supported_devices[0])
         except: raise RuntimeError("Unsupported device")
 
-#     def _get_optimal_block_size(self, op, matrix_dim):
-#         """ calculates best launch params for a given operation"""
-#         pass
-# 
-#     def _supports(self, feature):
-#         """ check compatability flags (eg: tensor core, FP16)"""
-#         pass
-#     
-#     def _benchmark(self):
-#         pass
+    def get_device_type(self):
+        system = platform.system()
+        if system == "Darwin": return "Metal"
+        # linux paths
+        for chip, (lib, init_fn) in CHIPS.items():
+            try:
+                dll = ctypes.CDLL(lib)
+                getattr(dll, init_fn)(0)
+                return chip
+            except:
+                continue
+        return "UNSUPPORTED"
+
+    def get_device_info(self, device_type: str):
+        """
+        Dynamically loads and initializes device ops based on the provided device type.
+
+        To add support for new chips:
+            1. Create supporting `ops_<chip>.py` file in src/forge/ops/ (ex: ops_amd.py)
+            2. Ensure the driver function is named `_device_info` in `_<Device>Ops` class
+            3. Create a corresponding Pydantic model at src/forge/models/
+                a. Update `self.gpu_info` in Forge class to support new model
+            4. Return the appropriate GPU model (ex: AMDGPU) from `_device_info()`
+            5. Add device lib and init functions to src/constants/device.py
+        """
+        try: 
+            # access file from ops if it matches the device type input
+            _ops_file = [file for file in (Path(__file__).parent.parent/"ops").iterdir() if file.stem.startswith("ops_") and file.stem[len("ops_"):].upper() == device_type]
+            module = importlib.import_module(f"forge.ops.{_ops_file[0].stem}")
+            cls = getattr(module, f"_{device_type.capitalize()}Ops")
+            req = cls()
+            return req._device_info()
+        except FileNotFoundError:
+            raise RuntimeError(f"No file found for device: {device_type}.")
+        except AttributeError:
+            raise RuntimeError(f"_{device_type.capitalize()}Ops class not found in module.")
+        except Exception as e:
+            raise RuntimeError(f"Failed to initialize device {device_type}: {e}")
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
     d = Device()
     # print(d.supported_devices)
     print(d._init_device())
+
+
+
+
+    # future methods for consideration 
+    #     def _get_optimal_block_size(self, op, matrix_dim):
+    #         """ calculates best launch params for a given operation"""
+    #         pass
+    # 
+    #     def _supports(self, feature):
+    #         """ check compatability flags (eg: tensor core, FP16)"""
+    #         pass
+    #     
+    #     def _benchmark(self):
+    #         pass
+
