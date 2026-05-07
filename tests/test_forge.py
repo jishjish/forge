8934a80da9752c846a6466088e28dd390f21fc5d
jishjish <josh@astrotek.app>
2026-05-07 15:59:15 -0400

    production test for forge device info

--- tests/test_forge.py
+++ tests/test_forge.py
@@ -8,3 +8,13 @@ def test_device_info_testing_env(monkeypatch):
     f = Forge()
     f._device_info()
     assert isinstance(f.gpu_info, NvidiaGPU)
+
+
+def test_device_info_production_env(monkeypatch):
+    monkeypatch.setenv("APP_SETTINGS", "production")
+    f = Forge()
+    mock_gpu = NvidiaGPU()
+    with patch.object(f.device, "get_device_info", return_value=mock_gpu):
+        f._device_info()
+        assert f.gpu_info == mock_gpu
+    
