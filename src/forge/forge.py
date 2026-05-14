1946d72862863aa5e6073a3b032d276a806aedfa
jishjish <josh@astrotek.app>
2026-05-14 19:01:25 -0400

    formatting

--- src/forge/forge.py
+++ src/forge/forge.py
@@ -18,6 +18,7 @@ class Forge:
     def __init__(self):
         self.device = Device()
         self.gpu_info = None
+        self._device_info()
         # self.graph = GraphBuilder(self.ast)
         # self.codegen = CUDACodegen(self.graph)
 
@@ -33,32 +34,29 @@ class Forge:
             gpu_info = self.device.get_device_info()
             assert isinstance(gpu_info, tuple(self._gpu_models)), f"Unsupported GPU: {type(gpu_info).__name__}. Supported: {[c.__name__ for c in self._gpu_models]}"
             self.gpu_info = gpu_info
-            if DEBUG >= 1: 
-                print(f"[dim]forge ({Path(__file__).name})[/dim] | [dim]Device initialized:[/dim] {self.gpu_info}")
+            if DEBUG >= 1: print(f"[dim]forge ({Path(__file__).name})[/dim] | [dim]Device initialized:[/dim] {self.gpu_info}")
         else: 
             self.gpu_info = _models.NvidiaGPU()
-            if DEBUG >= 1: 
-                print(f"[dim]forge ({Path(__file__).name})[/dim] | [white]env set to testing, returning base NVIDIA model[/white]")
+            if DEBUG >= 1: print(f"[dim]forge ({Path(__file__).name})[/dim] | [white]env set to testing, returning base NVIDIA model[/white]")
     
     def run(self, op: str, **kwargs):
         assert op in self._portfolio_ops or op in self._linalg_ops, \
             f"{op} not found. Supported ops: {self._portfolio_ops + self._linalg_ops}"
-
         # import bases on provided op; either through `portfolio` or `linalg`
         if op in self._portfolio_ops: import_path = f"forge.codegen.portfolio.op_{op}"
         else: import_path = f"forge.codegen.linalg.op_{op}"
         if DEBUG >= 1:
             print(f"[dim]forge ({Path(__file__).name})[/dim] | Import path: {import_path} for {op} operation")
             print(f"[dim]forge ({Path(__file__).name})[/dim] | Kwargs: {kwargs}")
-
         try: ops = importlib.import_module(import_path)
         except ModuleNotFoundError: raise RuntimeError(f"Op module not found: {import_path}")
        
 
 if __name__ == "__main__":
     f = Forge()
-    print(f._device_info()) 
+    # print(f._device_info()) 
     # print(f._gpu_models)
-    # print(f.run('matmul'))
+    print(f.gpu_info)
+    print(f.run("matmul"))
     # print(f.supported_ops())
     # print(DEBUG)
