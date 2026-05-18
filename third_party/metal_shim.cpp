12c4b39bdda9875a0342c20ad54cb6ae263bdb09
jishjish <josh@astrotek.app>
2026-05-18 17:32:13 -0400

    metal compilation and pipeline shim

--- third_party/metal_shim.cpp
+++ third_party/metal_shim.cpp
@@ -8,6 +8,7 @@ you use it to communicate with the GPU.
 Metal divides grids into smaller grids calldd `threadgroups`.
 */
 
+#include <iostream>
 #define NS_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
@@ -68,6 +69,37 @@ extern "C" {
     {
         if (!device) return 0.0;
         return ((MTL::Device*)device)->supportsFamily((MTL::GPUFamily)gpuFamily);
+    }
+
+    MTL::Function* forge_compile_source(void* device, const char* source_code, const char* function_name)
+    {
+        if (!device) { std::cerr << "No device present\n"; return nullptr;}
+        NS::String* src = NS::String::string(source_code, NS::StringEncoding::UTF8StringEncoding);
+        NS::Error* error = nullptr;
+        
+        // create new library with the source code
+        MTL::Library* lib = ((MTL::Device*)device)->newLibrary(src, nullptr, &error);
+        if (!lib) { std::cerr << "Issue creating new metal library";}
+        
+        // define function name
+        NS::String* func_name = NS::String::string(function_name, NS::StringEncoding::UTF8StringEncoding);
+
+        // generate the function
+        MTL::Function* func = lib->newFunction(func_name);
+
+        // release the func name
+        func_name->release();
+        if (!func) { std::cerr << "Failed to find shader function.\n";}
+
+        return func;
+    }
+
+    MTL::ComputePipelineState* forge_generate_pipeline(void* device, void* func)
+    {
+        NS::Error* pipeline_error = nullptr;
+        MTL::ComputePipelineState* pipeline = ((MTL::Device*)device)->newComputePipelineState((MTL::Function*)func, &pipeline_error);
+        if (!pipeline) { std::cerr << "Failed to generate pipeline";}
+        return pipeline;
     }
 }
 
