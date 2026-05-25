16364650beb5245bc1a3fab793fc642d791e9255
jishjish <josh@astrotek.app>
2026-05-25 17:39:38 -0400

    realize bool arg for printing results

--- src/forge/forge.py
+++ src/forge/forge.py
@@ -8,7 +8,6 @@ import forge.models as _models
 from dotenv import load_dotenv
 from .device.device import Device
 from forge.codegen.compile import Compile
-
 load_dotenv()
 
 class Forge:
@@ -40,7 +39,7 @@ class Forge:
             self.gpu_info = _models.NvidiaGPU()
             if DEBUG >= 1: print(f"[dim]forge ({Path(__file__).name})[/dim] | [white]env set to testing, returning base NVIDIA model[/white]")
     
-    def run(self, op: str, **kwargs):
+    def run(self, op: str, realize: bool = True, **kwargs):
         assert op in self._portfolio_ops or op in self._linalg_ops, \
             f"{op} not found. Supported ops: {self._portfolio_ops + self._linalg_ops}"
         # import bases on provided op; either through `portfolio` or `linalg`
@@ -52,13 +51,9 @@ class Forge:
         # start handling of compilations
         assert self.gpu_info is not None, "Device not initialized"
         comp = Compile(self.gpu_info, import_path, op)
-        comp.generate(**kwargs) 
+        comp.generate(realize=realize, **kwargs) 
 
 if __name__ == "__main__":
     f = Forge()
-    # print(f._device_info()) 
-    # print(f._gpu_models)
-    # print(f.gpu_info)
     data = np.random.uniform(100, 500, size=500_000).astype(np.float32)
     f.run("log_returns", data=data)
-    # print(f.supported_ops())
