c4c78eec817958f9d3af126db93565506a4b6434
jishjish <josh@astrotek.app>
2026-05-29 15:43:45 -0400

    realize logic

--- src/forge/forge.py
+++ src/forge/forge.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 import importlib
 from rich import print
 from pathlib import Path
@@ -7,7 +8,7 @@ import forge.models as _models
 from dotenv import load_dotenv
 from .device.device import Device
 from forge.codegen.compile import Compile
-from utils import available_gpu_models, available_portfolio_ops, available_linalg_ops, get_op_import_path
+from utils import available_gpu_models, available_portfolio_ops, available_linalg_ops, get_op_import_path 
 load_dotenv()
 
 class Forge:
@@ -19,6 +20,7 @@ class Forge:
         self.device = Device()
         self.gpu_info = None
         self._device_info()
+        self._results = {}
 
     @classmethod
     def supported_gpus(cls): print([m.__name__ for m in cls._gpu_models])
@@ -37,7 +39,7 @@ class Forge:
             self.gpu_info = _models.NvidiaGPU()
             if DEBUG >= 1: print(f"[dim]forge ({Path(__file__).name})[/dim] | [white]env set to testing, returning base NVIDIA model[/white]")
     
-    def run(self, op: str, realize: bool = True, **kwargs):
+    def run(self, op: str, realize: bool = False, **kwargs):
         assert op in self._portfolio_ops or op in self._linalg_ops, f"{op} not found. Supported ops: {self._portfolio_ops + self._linalg_ops}"
         import_path = get_op_import_path(op) # import based on provided op (thru portfolio or linalg)
 
@@ -50,39 +52,30 @@ class Forge:
         # start handling of compilations
         assert self.gpu_info is not None, "Device not initialized"
         comp = Compile(self.gpu_info, import_path, op, decomp_pipeline)
-        comp.generate(realize=realize, **kwargs) 
+        state, data_length, output_data_shape = comp.generate(realize=realize, **kwargs) 
+        handle = str(uuid.uuid4())
+        self._results[handle] = {"data_ptr": state, "data_length": data_length, "output_data_shape": output_data_shape}
+        return handle
+    
+    def realize(self, handle):
+        data_ptr_dict = self._results[handle]["data_ptr"]
+        data_ptr = list(data_ptr_dict.values())[-1]
+        output_data_shape = self._results[handle]["output_data_shape"]
+
+        # call realize() through ops to ffi
+        _ops_file = [file for file in (Path(__file__).parent/"ops").iterdir() if file.stem.startswith("ops_") and file.stem[len("ops_"):].upper() == self.gpu_info.device_type.upper()]
+        module = importlib.import_module(f"forge.ops.{_ops_file[0].stem}")
+        cls = getattr(module, f"_{self.gpu_info.device_type.capitalize()}Compile")
+        req = cls()
+        return req.realize(data_ptr, output_data_shape)
+       
+
 
 if __name__ == "__main__":
     f = Forge()
     import numpy as np
-    import pandas as pd
-    import polars as pl
-
-    # np.ndarray - single asset
-    # data = np.random.uniform(100, 500, size=50).astype(np.float32)
-    # f.run("mean", data = data, realize=True)
-
-    # np.ndarray - multi asset
     data=np.random.uniform(100, 500, size=(5, 5)).astype(np.float32)
-    # f.run("log_returns", data = data, realize=True)
-    f.run("mean", data = data, realize=True)
-# 
-#     # pd.Series - single asset
-#     data=pd.Series(np.random.uniform(100, 500, size=50), name="AAPL")
-#     f.run("mean", data = data, realize=False)
-# 
-#     # pd.DataFrame - multi asset
-#     data=pd.DataFrame(np.random.uniform(100, 500, size=(50, 5)), columns=["AAPL","GOOGL","MSFT","AMZN","TSLA"])
-#     f.run("mean", data = data, realize=False)
-# 
-#     # pl.Series - single asset
-#     data = pl.Series("AAPL", np.random.uniform(100, 500, size=50).astype(np.float32))
-#     f.run("mean", data = data, realize=False)
-# 
-#     # pl.DataFrame - multi asset
-#     data = pl.DataFrame({ticker: np.random.uniform(100, 500, size=50).astype(np.float32) 
-#                 for ticker in ["AAPL","GOOGL","MSFT","AMZN","TSLA"]})
-#     f.run("mean", data = data, realize=False)
-# 
-
-    # f.run("mean", data = data, realize=False)
+    # a = f.run("log_returns", data = data, realize=False)
+    test = f.run("mean", data = data, realize=False)
+    # print(f.realize(a))
+    # print(f.realize(test))
