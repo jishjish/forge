da3514b40d9e1c673ff36b9fdda9fb853aeab9b0
jishjish <josh@astrotek.app>
2026-06-18 12:51:58 -0400

    sql cache for kernel

--- src/forge/codegen/compile.py
+++ src/forge/codegen/compile.py
@@ -1,3 +1,5 @@
+import os
+import sqlite3
 import importlib
 import numpy as np
 import polars as pl
@@ -15,13 +17,20 @@ class Compile:
         self.gpu = gpu
         self.op_name = op_name
         self.decomp_pipeline = decomp_pipeline
-        # TODO: repurpose to cache path for sql lite for kernels
-        self.output_path = Path(__file__).resolve().parent / "source_code" / f"{op_name}{self.gpu.file_ext}"
+        self.output_path = Path(__file__).resolve().parent / "cached_code"
         self._ops_file = [file for file in (Path(__file__).parent.parent/"ops").iterdir() if file.stem.startswith("ops_") and file.stem[len("ops_"):].upper() == self.gpu.device_type.upper()]
         self.cleaned_data = self._handle_data(self.gpu, data)
         self.data_dim = self.cleaned_data[1]
+        
+        # get shapes throughtout the decomp pipeline
         shp = ShapeTracker(data_dim=self.cleaned_data[1], pipeline=self.decomp_pipeline)
         self.shapes = shp.calculate_output_shape()
+
+        # check for cached code throughout the decomp pipeline
+        self.cache = CodeCache(self.gpu, self.output_path)
+        self.ops = [op['op'] for op in self.decomp_pipeline]
+        self.cached_code = {}
+        for op in self.ops: self.cached_code[op] = {"source_code": self.cache.lookup_cache(op_name=op, original_data_shape=str((self.data_dim["entries"], self.data_dim["assets"])), device_name=self.gpu.name)}
         if DEBUG >= 1: print(f"[dim]forge ({Path(__file__).name})[/dim] \n | GPU: {self.gpu.device_type}, \n | out: {self.output_path}")
 
     def dispatch(self, source_code, func_name, data, spec_array, output_shape, realize, is_buffer):
@@ -30,6 +39,7 @@ class Compile:
         req = cls()
         buffer_alloc_data = extract_buffer_count_from_source(source_code)
         if DEBUG >= 1: print(f"[dim]forge ({Path(__file__).name})[/dim] \n | ops: {self._ops_file} \n | module: {module} \n | cls: {cls}")
+        self.cache.write_cache(op_name=func_name, op_code=source_code, original_data_shape=str((self.data_dim["entries"], self.data_dim["assets"])), device_name=self.gpu.name, threadgroup_size=None, execution_ns=None)
         return req._compile(source_code, func_name, self.gpu, data, spec_array, buffer_alloc_data, len(buffer_alloc_data), output_shape, realize, is_buffer)     # call compilation file from corresponding device
 
     def _handle_data(self, gpu: BaseModel, data):
@@ -54,7 +64,13 @@ class Compile:
     
     def generate(self, data, realize: bool = False):
         assert self.data_dim["entries"] >= 2, "Calculations require at least two datapoints."
-        fused_code = self._fuse_source_code()
+        # check cache for pre built kernels
+        if all(self.cached_code.get(op, {}).get("source_code") for op in self.ops):
+            fused_code = self.cached_code
+            if DEBUG >= 1: print(f"[dim]forge ({Path(__file__).name})[/dim] \n | Cache hit: {fused_code}")
+        else:
+            fused_code = self._fuse_source_code()
+
         prev_output = self.cleaned_data[0]
         is_buffer = False
         prev_shape = (self.data_dim["entries"], self.data_dim["assets"])
@@ -89,3 +105,81 @@ class Compile:
 
         if DEBUG >= 1: print(f"[dim]forge ({Path(__file__).name})[/dim] | Updated source code and wrote to: {self.output_path}")
         return res, spec_array.size, final_shape, self.shapes[-1]
+
+
+
+
+class CodeCache:
+    def __init__(self, gpu: BaseModel, output_path: Path):
+        self.gpu = gpu
+        self.output_path = output_path
+        self.db_name = "cached_code.db"
+        self.db_path = os.path.join(output_path, self.db_name)
+        self.table_name = "cached_ops"
+        self.connection = sqlite3.connect(self.db_path)
+        self.cursor = self.connection.cursor()
+        self._init_cache()
+
+    def _init_cache(self):
+        try:
+            self.cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS {self.table_name} (
+                op_name                     TEXT,                                           -- operation name
+                op_code                     TEXT,                                           -- source code
+                original_data_shape         TEXT,                                           -- data input shape "(4,3)"
+                device_name                 TEXT,                                           -- "Apple M1 Pro"
+                threadgroup_size            INTEGER,                         
+                execution_ns                INTEGER DEFAULT NULL,                           -- execution in nanoseconds
+                PRIMARY KEY                 (op_name, original_data_shape, device_name)
+            )
+            """)
+        except sqlite3.OperationalError as e:
+            print(f"Error generating table:\n {e}")
+
+    def lookup_cache(self, op_name: str, original_data_shape: str, device_name: str):
+        try: 
+            self.cursor.execute(f"""
+            SELECT op_code FROM {self.table_name} 
+            WHERE op_name = ? AND original_data_shape = ? AND device_name = ?
+            """, (op_name, original_data_shape, device_name))
+            result = self.cursor.fetchone()
+            return result[0] if result else None
+        except sqlite3.OperationalError as e:
+            print(f"Read error:\n {e}")
+
+    def write_cache(self, op_name, op_code, original_data_shape, device_name, threadgroup_size, execution_ns):
+        try: 
+            self.cursor.execute(f"""
+            INSERT OR REPLACE INTO {self.table_name} (op_name, op_code, original_data_shape, device_name, threadgroup_size, execution_ns)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """, (op_name, op_code, original_data_shape, device_name, threadgroup_size, execution_ns))
+            self.connection.commit()
+        except sqlite3.OperationalError as e:
+            print(f"Write error:\n {e}")
+
+    def close(self):
+        self.connection.close()
+
+    def read_all(self):
+        self.cursor.execute(f"SELECT * FROM {self.table_name}")
+        return self.cursor.fetchall()
+
+
+
+if __name__ == "__main__":
+    from src.forge.models import MetalGPU
+    gpu = MetalGPU(
+        device_type='Metal',
+        file_ext='.metal',
+        name='Apple M1 Pro',
+        maxThreadgroupMemoryLength=32768,
+        maxThreadsPerThreadgroupX=1024,
+        maxThreadsPerThreadgroupY=1024,
+        maxThreadsPerThreadgroupZ=1024,
+        recommendedMaxWorkingSetSize=12713115648,
+        supportsFamily='apple7'
+    )
+    output_path = Path('/Users/joshphillips/forge/src/forge/codegen/cached_code')
+    cc = CodeCache(gpu=gpu, output_path=output_path)
+    # print(cc.read_all())
+    print(cc.lookup_cache(op_name="log_returns", original_data_shape="(5, 2)", device_name="Apple M1 Pro"))
